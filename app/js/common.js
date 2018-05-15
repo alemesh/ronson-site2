209acaedb080e9fc73d9ffc774f56d3879bcc577
alemesh <alemesh@acceptic.com>
2018-05-15 19:06:05 +0300

    client changes

--- app/js/common.js
+++ app/js/common.js
@@ -24,6 +24,7 @@ $(function() {
 // Get the button that opens the modal
     var btn = document.getElementById("myBtn");
     var btn2 = document.getElementById("myBtn2");
+
     var btn3 = document.getElementsByClassName("myBtns")[0];
     var btn31 = document.getElementsByClassName("myBtns")[1];
     var btn4 = document.getElementsByClassName("myBtns")[2];
@@ -47,6 +48,25 @@ $(function() {
         modal.style.display = "block";
     };
 
+
+
+
+
+
+
+
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    };
 
 
     btn3.onclick = function() {
@@ -79,23 +99,6 @@ $(function() {
     btn71.onclick = function() {
         modal.style.display = "block";
     };
-
-
-
-
-
-// When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-// When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-
 //============ finish modal ===============
 
 
